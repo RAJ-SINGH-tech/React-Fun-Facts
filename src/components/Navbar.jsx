eef8046bf88d85f664b3dac4a6424ca075888862
rajsingh <rajsingh23.1.2001@gmail.com>
2023-04-08 18:04:39 +0530

    first version of the facts app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <nav>
+        <img src="../src/assets/logo.svg" />
+        <h2>ReactFacts</h2>
+        <h4>React Course Project - 1</h4>
+    </nav>
+  )
+}
+
+export default Navbar
